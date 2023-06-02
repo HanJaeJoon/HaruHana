954ef3065adbee104aac889063362f66af58aded
jaejoon.han <jaejoon.han@crevisse.com>
2023-06-03 00:01:22 +0900

    Google OAuth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,8 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github"
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
@@ -47,9 +48,9 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
